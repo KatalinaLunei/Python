33d7841cd0ba20d6c7d2473c1002c0410c024a93
Ekaterina_Popova <agekaterina@list.ru>
2023-06-20 15:34:10 +0300

    task 44

---
+++ Задача 44.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import random 
+lst = ['robot'] * 10  
+lst += ['human'] * 10  
+random.shuffle(lst)  
+data = pd.DataFrame({'human': data['whoAmI'] == 'human',
+              'robot': data['whoAmI'] == 'robot'}).astype(int).head(10)
+data.head(10)
